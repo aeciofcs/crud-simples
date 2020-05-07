682c6b351bccd34efea0f6d48a188f9cf9ed1d13
Aecio Silva <aeciofcs@gmail.com>
2020-05-07 17:14:36 -0300

    Criação Componente Logo, Imagem Logo e Estilos

--- frontend/src/components/templates/Logo.jsx
+++ frontend/src/components/templates/Logo.jsx
@@ -1,7 +1,11 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo.jpeg'
 import React from 'react'
 
 export default props =>
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
+
     </aside>
