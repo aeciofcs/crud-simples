f308f57aa7d189d8d9beeb0a4298cdafc2840459
Aecio Silva <aeciofcs@gmail.com>
2020-05-08 19:03:49 -0300

    Ajuste no Comp Home e aplicado estilo CSS.

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props => 
+    <Main icon="home" title="Inicio"
+        subtitle="### CRUD SIMPLES PARA USUÁRIOS ###">
+        <div className="display-4">BEM VINDO!</div>
+        <hr />
+        <p className="mb-8">Sistema para exemplificar
+        a construção de um cadastro desenvolvido em
+            React. </p>
+    </Main>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -6,15 +6,14 @@ import React from 'react'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
-import Main from '../components/templates/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/templates/Footer'
 
 export default props => 
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" title="Inicio" 
-              subtitle="### CRUD SIMPLES PARA USUÁRIOS ###" />
+        <Home />
         <Footer />
     </div>
 
