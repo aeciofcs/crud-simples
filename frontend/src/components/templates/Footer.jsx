eebc67d7851c8175faa87b9eab6e719a91d612d4
Aecio Silva <aeciofcs@gmail.com>
2020-05-08 18:29:17 -0300

    Ajustando o comp Footer e aplicando estilo css.

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -3,5 +3,14 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido de <i className="fa fa-heart text-danger"></i> por
+            <strong>
+                <span className="text-danger"> A</span>
+                <span className="text-primary">e</span>
+                <span className="text-danger">c</span>
+                <span className="text-primary">i</span>
+                <span className="text-danger">o</span>
+            </strong>
+        </span>
     </footer>
